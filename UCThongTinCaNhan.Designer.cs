a1c888e03fc89667d0dfc85752521dc7971c34f9
khaCrz <80761070+khaCrz@users.noreply.github.com>
2021-05-27 20:24:43 +0700

    Infor

---
+++ UCThongTinCaNhan.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace QLThu_Vien1
+{
+    partial class UCThongTinCaNhan
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_Name = new System.Windows.Forms.Label();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.label_Phone = new System.Windows.Forms.Label();
+            this.label_NS = new System.Windows.Forms.Label();
+            this.label_Gender = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label_CMND = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(116, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label3.Location = new System.Drawing.Point(113, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label5.Location = new System.Drawing.Point(563, 100);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "NS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label6.Location = new System.Drawing.Point(116, 205);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 28);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // label_Name
+            // 
+            this.label_Name.AutoSize = true;
+            this.label_Name.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Name.Location = new System.Drawing.Point(229, 100);
+            this.label_Name.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_Name.Name = "label_Name";
+            this.label_Name.Size = new System.Drawing.Size(63, 25);
+            this.label_Name.TabIndex = 6;
+            this.label_Name.Text = "label7";
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Email.Location = new System.Drawing.Point(229, 208);
+            this.label_Email.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(63, 25);
+            this.label_Email.TabIndex = 8;
+            this.label_Email.Text = "label9";
+            // 
+            // label_Phone
+            // 
+            this.label_Phone.AutoSize = true;
+            this.label_Phone.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Phone.Location = new System.Drawing.Point(229, 151);
+            this.label_Phone.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_Phone.Name = "label_Phone";
+            this.label_Phone.Size = new System.Drawing.Size(63, 25);
+            this.label_Phone.TabIndex = 8;
+            this.label_Phone.Text = "label9";
+            // 
+            // label_NS
+            // 
+            this.label_NS.AutoSize = true;
+            this.label_NS.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NS.Location = new System.Drawing.Point(666, 100);
+            this.label_NS.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_NS.Name = "label_NS";
+            this.label_NS.Size = new System.Drawing.Size(73, 25);
+            this.label_NS.TabIndex = 9;
+            this.label_NS.Text = "label11";
+            // 
+            // label_Gender
+            // 
+            this.label_Gender.AutoSize = true;
+            this.label_Gender.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Gender.Location = new System.Drawing.Point(666, 148);
+            this.label_Gender.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_Gender.Name = "label_Gender";
+            this.label_Gender.Size = new System.Drawing.Size(73, 25);
+            this.label_Gender.TabIndex = 10;
+            this.label_Gender.Text = "label12";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 375);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 30);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Chỉnh sửa thông tin cá nhân";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label7.Location = new System.Drawing.Point(563, 202);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 28);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "CMND";
+            // 
+            // label_CMND
+            // 
+            this.label_CMND.AutoSize = true;
+            this.label_CMND.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_CMND.Location = new System.Drawing.Point(666, 205);
+            this.label_CMND.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_CMND.Name = "label_CMND";
+            this.label_CMND.Size = new System.Drawing.Size(73, 25);
+            this.label_CMND.TabIndex = 13;
+            this.label_CMND.Text = "label12";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label8.Location = new System.Drawing.Point(563, 148);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 28);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Gender";
+            // 
+            // UCThongTinCaNhan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label_CMND);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_Gender);
+            this.Controls.Add(this.label_NS);
+            this.Controls.Add(this.label_Phone);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.label_Name);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "UCThongTinCaNhan";
+            this.Size = new System.Drawing.Size(909, 479);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_Name;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.Label label_Phone;
+        private System.Windows.Forms.Label label_NS;
+        private System.Windows.Forms.Label label_Gender;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label_CMND;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ UCThongTinCaNhan.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThu_Vien1
+{
+    public partial class UCThongTinCaNhan : UserControl
+    {
+        private int _ID;
+        private static UCThongTinCaNhan _Instance;
+        public static UCThongTinCaNhan Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new UCThongTinCaNhan();
+                }
+                return _Instance;
+            }
+        }
+        public UCThongTinCaNhan()
+        {
+            InitializeComponent();
+        }
+        public void GetID(int ID)
+        {
+            _ID = ID;
+            Member member = BLL._Instance.GetMemberByID(ID);
+            label_Name.Text = member.Name.Trim();
+            label_Gender.Text = member.Gender.Trim();
+            label_CMND.Text = member.CMND.Trim();
+            label_Email.Text = member.Email.Trim();
+            label_NS.Text = member.NS.Value.ToShortDateString();
+            label_Phone.Text = member.Phone.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form_DesignInfor FD = new Form_DesignInfor(_ID);
+            FD.FormClosed += close;
+        }
+        public void close(object sender, EventArgs e)
+        {
+            GetID(_ID);
+        }
+    }
+}
