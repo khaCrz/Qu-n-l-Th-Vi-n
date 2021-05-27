ba27b156def4cad9ce8c4a8b4ea284b1b686dc5a
Lê Lộc <72958498+deathnotec@users.noreply.github.com>
2021-05-27 20:50:41 +0700

    Edit user

---
+++ QLThu_Vien/EditUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThu_Vien1
+{
+    public partial class EditUser : Form
+    {
+        public EditUser()
+        {
+            InitializeComponent();
+            load();
+        }
+        public void load()
+        {
+            dataGridView1.DataSource = BLL._Instance.GetAllMember();
+        }
+    }
+}
