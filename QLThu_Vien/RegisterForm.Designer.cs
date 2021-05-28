f030017f06e87eaf0a935d05dd8d99a07a135620
khaCrz <80761070+khaCrz@users.noreply.github.com>
2021-05-28 11:51:53 +0700

    Login _ register

---
+++ QLThu_Vien/RegisterForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace QLThu_Vien1
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_passwordConfirm = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.textBox_username.Location = new System.Drawing.Point(365, 67);
+            this.textBox_username.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(300, 38);
+            this.textBox_username.TabIndex = 0;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.textBox_password.Location = new System.Drawing.Point(365, 129);
+            this.textBox_password.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(300, 38);
+            this.textBox_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.label1.Location = new System.Drawing.Point(119, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.label2.Location = new System.Drawing.Point(119, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(365, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(274, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(307, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mật khẩu phải chứa 3 ký tự";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.label4.Location = new System.Drawing.Point(119, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Confirm Password";
+            // 
+            // textBox_passwordConfirm
+            // 
+            this.textBox_passwordConfirm.Font = new System.Drawing.Font("Segoe UI", 13.8F);
+            this.textBox_passwordConfirm.Location = new System.Drawing.Point(365, 189);
+            this.textBox_passwordConfirm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_passwordConfirm.Name = "textBox_passwordConfirm";
+            this.textBox_passwordConfirm.Size = new System.Drawing.Size(300, 38);
+            this.textBox_passwordConfirm.TabIndex = 9;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 432);
+            this.Controls.Add(this.textBox_passwordConfirm);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_username);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "RegisterForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_passwordConfirm;
+    }
+}
